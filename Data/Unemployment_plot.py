bdc860fe3e7f23c061d2b711cdfd0ae1722b1fba
Sebin Sunny <me@sebinsunny.com>
2020-05-21 23:47:36 +1000

    update the Australia graph into the website

--- Data/Unemployment_plot.py
+++ Data/Unemployment_plot.py
@@ -9,28 +9,27 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-plt.rcParams['figure.figsize']=(20,10) 
- # using a theme for better visualization
+plt.rcParams['figure.figsize'] = (20, 10)
+# using a theme for better visualization
 plt.style.use('fivethirtyeight')
 # Read the AU Dataset
-AU_Final = pd.read_csv('/Users/sebin/Desktop/companyandngo/Data/Datasets/AUFinal.csv')
-
+AU_Final = pd.read_csv('/Users/sebin/Desktop/companyandngo/Data/Datasets/AUFinal.csv')[::-1]
 
 # In[5]:
 
 
-AU_Final.Date = pd.to_datetime(AU_Final.Date) 
-AU_Final.set_index('Date', inplace=True) 
+AU_Final.Date = pd.to_datetime(AU_Final.Date)
+AU_Final.set_index('Date', inplace=True)
 
 
 # In[72]:
 
 
-def draw_linechart(i):    
-    ax1.clear()      
+def draw_linechart(i):
+    ax1.clear()
     ax2 = ax1.twinx()
     ax1.plot(AU_Final['Unemployment_Rate'][:i], color="purple")
-    ax2.bar(AU_Final.index, AU_Final.Recession,width=20, alpha=0.4, color='red') #second axis
+    ax2.bar(AU_Final.index, AU_Final.Recession, width=20, alpha=0.4, color='red')  # second axis
     ax2.grid(b=False)
     ax1.grid(b=False)
     ax1.set_title('Unemployment Rate Variation')
